4e006208a56e6aeb822e38151c7d70f7094c40b3
Xavier Er <xavier_er@live.com.sg>
2016-10-18 03:30:03 +0800

    Update

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var orders = require('./routes/orders');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -38,4 +40,6 @@ app.use('/api/orders', orders);
 app.use('/src', express.static(__dirname + '/public/src'));
 app.use('/css', express.static(__dirname + '/public/css'));
 // app.use(express.static('public'));
-app.listen(3000)
+app.listen(app.get('port'), function () {
+    console.log('Node app is running on port', app.get('port'));
+});
